00d42baf038b7775e03f8046557f0a31cb867f71
deepakskepit <148887247+deepakskepit@users.noreply.github.com>
2024-01-03 11:54:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import base64
 # GitHub repository information
 GITHUB_REPO_OWNER = "deepakskepit"
 GITHUB_REPO_NAME = "test_automate"
-GITHUB_ACCESS_TOKEN = "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"  # Make sure to keep this secure
+GITHUB_ACCESS_TOKEN = "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"  # Make sure to keep this secure
 
 # Function to create or load the user data Excel file
 def initialize_user_data():
